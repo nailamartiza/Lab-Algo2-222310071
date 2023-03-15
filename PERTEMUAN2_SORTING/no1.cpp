508eae450fac81679ce407bb86b8e2abdc2cfbcc
nailamartiza <113446150+nailamartiza@users.noreply.github.com>
2023-03-15 21:21:30 +0700

    Add files via upload

---
+++ PERTEMUAN2_SORTING/no1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+//Fungsi penukaran nilai
+void swap(int arr[], int pos1, int pos2){
+  int temp;
+  temp = arr[pos1];
+  arr[pos1] = arr[pos2];
+  arr[pos2] = temp;
+}
+//partisi array
+int partition (int arr[], int low, int high, int pivot){
+  int i = low;
+  int j = low;
+  while (i<=high){
+    if (arr[i]>pivot){
+      i++;
+    }
+    else{
+      swap(arr, i, j);
+      i++;
+      j++;
+    }
+  }
+  return j-1;
+}
+//Fungsi quicksort
+void quicksort (int arr[], int low, int high){
+  if (low<high){
+    int pivot = arr[high];
+    int pos = partition(arr, low, high, pivot);//pos adalah indeks partisi
+
+    quicksort(arr, low, pos-1);//Quicksort pada bagian sebelum partisi
+    quicksort(arr, pos+1, high);//Quicksort pada bagian setelah partisi
+  }
+}
+
+int main() {
+  //Menginput data array
+  int n;
+  cout << "Tentukan panjang array: ";
+  cin >> n;
+  int arr[n];
+  for(int i=0; i<n; i++){
+    cin >> arr[i];
+  }
+  //Data array yang diurutkan
+  quicksort(arr, 0, n-1);
+  cout << "Berikut adalah array yang telah diurutkan: ";
+  for (int i=0; i<n; i++){
+    cout << arr[i] << "\t";
+  }
+}
